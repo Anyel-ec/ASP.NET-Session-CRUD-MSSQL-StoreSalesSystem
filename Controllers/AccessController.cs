a531dbb372a8617f2617d8f0aeabac409087af7f
anyel-ec <paulangel826@gmail.com>
2024-03-02 19:55:51 -0500

    add filter to Session User in loggin

---
+++ Controllers/AccessController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TiendaCRUD.Controllers
+{
+    public class AccessController : Controller
+    {
+        // GET: Access
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Filters/VerifySession.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TiendaCRUD.Models;
+using System.Web.Mvc;
+
+namespace TiendaCRUD.Filters
+{
+    public class VerifySession : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            try
+            {
+                var oUsuario = (usuario)HttpContext.Current.Session["User"];
+                base.OnActionExecuting(filterContext);
+                if (filterContext.HttpContext.Session["User"] == null)
+                {
+                    if (filterContext.Controller is Controllers.AccessController == false)
+                    {
+                        filterContext.HttpContext.Response.Redirect("~/Access/Index");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                filterContext.Result = new RedirectToRouteResult(new System.Web.Routing.RouteValueDictionary { { "Controller", "Access" }, { "Action", "Index" } });
+            }
+        }   
+    }
+}
